f78dd27a5040dbf13e0a9e29d3148dcae5f622c7
Ashwin Athrey Yellapantula <ashwinathrey93@gmail.com>
2019-06-14 11:44:19 -0400

    Update emailNotifier.groovy

--- vars/emailNotifier.groovy
+++ vars/emailNotifier.groovy
@@ -1,8 +1,8 @@
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    emailtext subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]': Check console output at '${env.BUILD_URL}' ", to: '$DEFAULT_RECIPIENTS'
+    emailext subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]': Check console output at '${env.BUILD_URL}' ", to: '$DEFAULT_RECIPIENTS'
   }
   else if( buildResult == "FAILURE" ) { 
-    emailtext subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]': Check console output at '${env.BUILD_URL}' ", to: '$DEFAULT_RECIPIENTS'
+    emailext subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]': Check console output at '${env.BUILD_URL}' ", to: '$DEFAULT_RECIPIENTS'
     }
 }
